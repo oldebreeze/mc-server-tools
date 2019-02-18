66b404156e2d484b6755170bdb116d562e846207
Pete <pete.olbrys@gmail.com>
2019-02-18 17:20:34 -0500

    commiting latest changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mc-server-tools
+
+Collection of experimental tools for building a Minecraft server and learning Terraform.
+
+
+
+** TODO **
--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "minecraft" {
-    ami = "ami-0970ab4b97fe3a913"
-    key_name = "pete-aws-personal"
+    ami           = "ami-0970ab4b97fe3a913"
+    key_name      = "pete-aws-personal"
     instance_type = "t2.micro"
 
     security_groups = [
@@ -16,27 +16,49 @@ resource "aws_instance" "minecraft" {
         Name = "minecraft"
     }
 
-    provisioner "remote_exec" {
+    provisioner "remote-exec" "add_user" {
         inline = [
             "sudo useradd -m -d /home/minecraft/ -s /bin/bash minecraft",
             "sudo mkdir /usr/bin/minecraft",
         ]
+        connection = {
+            type        = "ssh"
+            user        = "ubuntu"
+            private_key = "${file("/home/pete/.ssh/pete-aws-personal.pem")}"
+        }
     }
 
-    provisioner "file" {
+    provisioner "file" "add_env_vars" {
         source      = "mc_env_vars.txt"
-        destination = "/home/minecraft/mc_env_vars.txt"
+        destination = "/tmp/mc_env_vars.txt"
+        connection = {
+            type        = "ssh"
+            user        = "ubuntu"
+            private_key = "${file("/home/pete/.ssh/pete-aws-personal.pem")}"
+        }
     }
     
-    provisoner "file" {
+    provisioner "file" "add_bin_files" {
         source      = "files/"
-        destination = "/usr/bin/minecraft/"
+        destination = "/tmp/"
+        connection = {
+            type        = "ssh"
+            user        = "ubuntu"
+            private_key = "${file("/home/pete/.ssh/pete-aws-personal.pem")}"
+        }
     }
 
-    provisoner "remote_exec" {
+    provisioner "remote-exec" "set_perms_run_install" {
         inline = [
+            "sudo mv /tmp/mc_env_vars.txt /home/minecraft/mc_env_vars.txt",
             "chown -R minecraft: /usr/bin/minecraft/",
             "chmod +x /usr/bin/minecraft/*"
+#            "/usr/bin/minecraft/minecraft_install.sh"
         ]
+        connection = {
+            type        = "ssh"
+            user        = "ubuntu"
+            private_key = "${file("/home/pete/.ssh/pete-aws-personal.pem")}"
+        }
     }
 }
